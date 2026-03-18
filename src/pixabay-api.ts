9ce23c9144211f0df159a3cd0a602ff435d87461
YehorNosyk <y.nosyk@student.bachelor-goit.com.ua>
2026-03-18 18:01:56 +0200

    Update pixabay-api.ts

--- src/pixabay-api.ts
+++ src/pixabay-api.ts
@@ -1,18 +1,9 @@
-import axios from "axios";
-import type { PixabayResponse } from "./types/pixabay";
-import { PER_PAGE } from "./pagination";
+import axios from 'axios';
+import type { PixabayResponse } from './types/pixabay';
 
-export const getImagesByQuery = async (query, page) => {
-  const response = await axios.get(`https://pixabay.com/api/`, {
-    params: {
-      q: query,
-      page,
-      per_page: PER_PAGE,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: true,
-      key: import.meta.env.VITE_PIXABAY_API_KEY,
-    },
+export async function getImagesByQuery(query: string, page: number): Promise<PixabayResponse> {
+  const response = await axios.get<PixabayResponse>('https://pixabay.com/api/', {
+    params: { key: 'API_KEY', q: query, page, per_page: 12 }
   });
   return response.data;
-};
+}
