9a339df01d800a40bc9d0e26107616324b2ddcbd
YehorNosyk <y.nosyk@student.bachelor-goit.com.ua>
2026-03-18 18:30:13 +0200

    Update simplelightbox.d.ts

--- src/types/simplelightbox.d.ts
+++ src/types/simplelightbox.d.ts
@@ -1,1 +1,19 @@
-declare module "simplelightbox" {}
+declare module 'simplelightbox' {
+  export interface SimpleLightboxOptions {
+    captions?: boolean;
+    captionsData?: 'alt' | 'title' | 'data-title';
+    loop?: boolean;
+    nav?: boolean;
+  }
+
+  export default class SimpleLightbox {
+    constructor(selector: string, options?: SimpleLightboxOptions);
+    refresh(): void;
+    open(position?: number): void;
+    next(): void;
+    prev(): void;
+    destroy(): void;
+    on(event: string, callback: () => void): void;
+    off(event: string, callback: () => void): void;
+  }
+}
